34be17941cb4778b2d1cc223d56bd539a94de648
Djay-M <dhananjay.m1805@gmail.com>
2025-08-09 20:46:28 +0530

    added a simple AI chat-bot

---
+++ agents/AI_Agents/1.simple_bot.py
@@ -0,0 +1,35 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+import os
+
+class AgentState(TypedDict):
+    messages: list[HumanMessage]
+
+llm = ChatOllama(model="llama3.1")
+
+def process_message(state: AgentState) -> AgentState:
+    """Node to process the human message to LLM"""
+
+    if state["messages"]:
+        ai_response = llm.invoke(state["messages"])
+        print("\n AI :: ", ai_response.content)
+
+    return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("process_message", process_message)
+
+graph.add_edge(START, "process_message")
+graph.add_edge("process_message", END)
+
+agent = graph.compile()
+
+user_input = input("\n Enter :: ")
+
+while user_input not in ["exit", "EXIT", "STOP", "stop", "end", "END"]:
+    agent.invoke({ "messages": user_input})
+    user_input = input("\n Enter :: ")
