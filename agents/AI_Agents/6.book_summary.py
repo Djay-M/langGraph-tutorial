965f8be5fe43892c028c40717336771bf37d167d
Djay-M <dhananjay.m1805@gmail.com>
2025-08-16 23:46:20 +0530

    inital code for loading pdf

--- agents/AI_Agents/6.book_summary.py
+++ agents/AI_Agents/6.book_summary.py
@@ -23,3 +23,11 @@ from langchain_chroma import Chroma
 # setup the LLM model and the embedding
 LLM = ChatOllama(model="llama3.1", temperature=0)
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+# Loading PDF files
+pdf_file_path = ""
+
+if not os.path.exists(pdf_file_path):
+    raise FileNotFoundError(f"No PDF file found on the path, please check the path")
+
+pdf_loader = PyPDFLoader(pdf_file_path)
